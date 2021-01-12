cef3075cbe0689d76ed048846ac758f9feaa878b
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-01-12 13:13:01 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a + b <= c || b + c <= a || a + c <= b || a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    elsif a == b && b == c
+      :equilateral
+    elsif
+      a == b || a == c || b == c
+      :isosceles
+    else
+      :scalene
+    end
+
+  end
+  class TriangleError < StandardError
+    message = "That's not a triangle, dummy!"
+  end
+
+
+
 end
